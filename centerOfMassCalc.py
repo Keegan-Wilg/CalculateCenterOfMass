68914194b8b4de8f1d2cc32ebae46dde22364e9c
Keegan <140941086+Keegan-Wilg@users.noreply.github.com>
2025-02-09 16:19:57 +1300

    Add files via upload

--- centerOfMassCalc.py
+++ centerOfMassCalc.py
@@ -5,7 +5,7 @@ print('\n' + f.renderText('CenterOfMass'))
 
 def calc_center_of_mass(object_count, object_mass_list, object_location_list):
     if object_mass_list.len() or object_location_list.len() != object_count:
-        raise Exception("Lists must be the same length as the amount of objects")
+        raise IndexError("Lists must be the same length as the amount of objects")
 
     total_mass = 0
     buffer_list = []
@@ -18,13 +18,13 @@ def calc_center_of_mass(object_count, object_mass_list, object_location_list):
             buffer_list.append(object_mass_list[iteration] * object_location_list[iteration])
             iteration += 1
         except:
-            raise Exception("Lists must only contain numbers")
+            raise ValueError("Lists must only contain numbers")
         
     for item in object_location_list:
         try:
             item + 1
         except:
-            raise Exception("Lists must only contain numbers")
+            raise ValueError("Lists must only contain numbers")
 
     for item in buffer_list:
         numerator_num = numerator_num + item
